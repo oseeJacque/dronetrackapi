7f5cf03cfdae10523fc763e43cbad63937646f21
oseeJacques <oseesoke@gmail.com>
2023-07-14 23:44:25 +0100

    install app name

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'oseesoke'
 
 def  allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS

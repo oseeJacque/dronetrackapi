a819710244a8823c0cfa03fa2a63c1784778e856
oseeJacque <91032220+oseeJacque@users.noreply.github.com>
2023-07-15 14:20:18 +0100

    Update app.py
    add host

--- app.py
+++ app.py
@@ -56,6 +56,6 @@ def upload_image():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
